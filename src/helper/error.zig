0b175e27c4040430eaffd17174c486ef4bed0142
TheDevConnor <connorharris427@gmail.com>
2024-04-02 15:34:26 -0400

    Updated the look of error messages

--- src/helper/error.zig
+++ src/helper/error.zig
@@ -14,6 +14,56 @@ fn printCharOrPlace(c: u8) void {
     }
 }
 
+fn printIgnore(line: usize) void {
+    if (line < 10) print("   ", .{});
+    if (line >= 10 and line < 100) print("   ", .{});
+    if (line >= 100) print("    ", .{});
+    if (line >= 1000) print("     ", .{});
+    if (line >= 10000) print("      ", .{});
+    if (line >= 100000) print("       ", .{});
+    if (line >= 1000000) print("        ", .{});
+}
+
+fn lineNumber(line: usize) []const u8 {
+    if (line < 10) {
+        return "0";
+    }
+    return "";
+}
+
+fn printLine(start: []const u8, line: usize) void {
+    var end = start;
+    while (end.len > 0 and end[0] != '\n' and end[0] != 0)
+        end = end[1..];
+    const lineLength = @intFromPtr(end.ptr) - @intFromPtr(start.ptr);
+    print("{s}{}|{s}\n", .{ lineNumber(line), line, start[0..lineLength] });
+}
+
+fn lineBefore(line: usize) !void {
+    if (line > 0) {
+        var start = try lineStart(line, token.scanner.source);
+        printLine(start, line);
+    }
+}
+
+fn currentLine(line: usize, pos: usize) !void {
+    comptime var cham = Chameleon.init(.Auto);
+    var start = try lineStart(line, token.scanner.source);
+    printLine(start, line);
+
+    // show where the error is!
+    var i: usize = 0;
+    printIgnore(line); // ignore the 01|
+    while (i < (pos - 1)) : (i += 1)
+        printCharOrPlace(start[i]);
+    print(cham.red().fmt("^\n"), .{});
+}
+
+fn lineAfter(line: usize) !void {
+    var start = try lineStart(line, token.scanner.source);
+    printLine(start, line);
+}
+
 pub fn Error(
     line: usize,
     pos: usize,
@@ -26,18 +76,7 @@ pub fn Error(
     print(cham.red().fmt(typeOfError), .{});
     print(" {s}\n", .{msg});
 
-    // Print our the line
-    var start = try lineStart(line, token.scanner.source);
-    var end = start;
-
-    while (end.len > 0 and end[0] != '\n' and end[0] != 0)
-        end = end[1..];
-    const lineLength = @intFromPtr(end.ptr) - @intFromPtr(start.ptr);
-    print("{s}\n", .{start[0..lineLength]});
-
-    // show where the error is!
-    var i: usize = 0;
-    while (i < (pos)) : (i += 1)
-        printCharOrPlace(start[i]);
-    print(cham.red().fmt("^\n"), .{});
+    try lineBefore(line - 1);
+    try currentLine(line, pos);
+    try lineAfter(line + 1);
 }
--- src/lexer/lexerHelper.zig
+++ src/lexer/lexerHelper.zig
@@ -49,7 +49,13 @@ pub fn makeToken(kind: tokens.TokenType) !Token {
 }
 
 pub fn errorToken(message: []const u8) !Token {
-    try lError.Error(tokens.scanner.line, tokens.scanner.pos - 1, message, "LEXER ERROR", tokens.scanner.filename);
+    try lError.Error(
+        tokens.scanner.line,
+        tokens.scanner.pos - 1,
+        message,
+        "LEXER ERROR",
+        tokens.scanner.filename,
+    );
     return makeToken(tokens.TokenType.Error);
 }
 
