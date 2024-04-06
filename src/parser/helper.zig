b833b0e88c1b284ddd585b7a4331d6bb446cfed5
TheDevConnor <connorharris2004@gmail.com>
2024-04-06 08:21:26 -0400

    Refactored the code a bit

--- src/parser/helper.zig
+++ src/parser/helper.zig
@@ -2,52 +2,35 @@ const std = @import("std");
 
 const prec = @import("prec.zig");
 const token = @import("../lexer/tokens.zig");
-const pError = @import("../helper/error.zig");
 const lexer = @import("../lexer/lexer.zig");
-const ast = @import("../ast/ast.zig");
 
-pub fn parseExpr(bp: prec.bp) ast.Expr {
-    var left = prec.nud_lookup(token.token.type);
+const Parser = struct {
+    allocator: std.mem.Allocator,
+    tks: std.ArrayList(token.Token),
+    index: usize,
+};
 
-    // We need to check the current tokens bp and compare it to the left bp
-    while (prec.bp_lookup(token.token.type).?.bp > bp) {
-        left = prec.led_lookup(token.token.type, left);
-    }
-
-    return left;
-}
-
-pub fn parsePrimary() ast.Expr {
-    const tok = token.token.type;
-    switch (tok) {
-        .Num => {
-            return ast.expr.Number{ .value = token.token.value };
-        },
-        .String => {
-            return ast.expr.String{ .value = token.token.value };
-        },
-        .Ident => {
-            return ast.expr.Ident{ .value = token.token.value };
-        },
-        else => {
-            pError.Error(
-                token.token.line,
-                token.token.pos,
-                "Can not create parser_primary!",
-                "PARSER ERROR",
-                token.scanner.filename,
-            );
-        },
-    }
-}
-
-pub fn parseBinary(left: ast.Expr) ast.Expr {
-    var op = token.token.type;
-    var right = parseExpr(.defalt);
-
-    return ast.expr.Binary{
-        .left = left,
-        .op = op,
-        .right = right,
+pub fn init_parser(allocator: std.mem.Allocator) Parser {
+    return Parser{
+        .allocator = allocator,
+        .tks = std.ArrayList(token.Token).init(allocator),
+        .index = 0,
     };
+}
+
+pub fn storeToken(parser: *Parser, tk: token.Token) !void {
+    _ = try lexer.scanToken(); // get ride if the start token
+    while (token.token.type != token.TokenType.Eof) {
+        try parser.tks.append(tk);
+        _ = try lexer.scanToken();
+    }
+}
+
+pub fn current(parser: *Parser) token.Token {
+    return parser.tks.items[parser.index];
+}
+
+pub fn next(parser: *Parser) token.Token {
+    parser.index += 1;
+    return parser.tks.items[parser.index];
 }
--- src/parser/parser.zig
+++ src/parser/parser.zig
@@ -1,53 +1,17 @@
 const std = @import("std");
 
 const token = @import("../lexer/tokens.zig");
-const scan = @import("../lexer/lexer.zig").scanToken;
-const pError = @import("../helper/error.zig");
-const helper = @import("helper.zig");
-const prec = @import("prec.zig");
-
-pub const Parser = struct {
-    allocator: std.mem.Allocator,
-    tks: std.ArrayList(token.Token),
-    index: usize,
-};
-
-fn init_parser(allocator: std.mem.Allocator) Parser {
-    return Parser{
-        .allocator = allocator,
-        .tks = std.ArrayList(token.Token).init(allocator),
-        .index = 0,
-    };
-}
-
-fn storeToken(parser: *Parser, tk: token.Token) !void {
-    _ = try scan(); // get ride if the start token
-    while (token.token.type != token.TokenType.Eof) {
-        try parser.tks.append(tk);
-        _ = try scan();
-    }
-}
-
-fn current(parser: *Parser) token.Token {
-    return parser.tks.items[parser.index];
-}
-
-fn next(parser: *Parser) token.Token {
-    parser.index += 1;
-    return parser.tks.items[parser.index];
-}
+const h = @import("helper.zig");
 
 pub fn parse(allocator: std.mem.Allocator) !void {
-    var parser = init_parser(allocator);
+    var parser = h.init_parser(allocator);
 
-    try storeToken(&parser, token.token);
+    try h.storeToken(&parser, token.token);
 
     while (parser.index < parser.tks.items.len) {
-        std.debug.print("{s}", .{current(&parser).value});
-        if (parser.index + 1 < parser.tks.items.len) {
-            _ = next(&parser);
-        } else {
-            break;
-        }
+        std.debug.print("{s}", .{h.current(&parser).value});
+
+        if (parser.index + 1 >= parser.tks.items.len) break;
+        _ = h.next(&parser);
     }
 }
